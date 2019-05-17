194555325efe1ed069e6cc0e39150338830ebba9
Nik <nik@drnik.org>
2019-05-17 18:30:50 +0200

    Make test a real test
    Show that using proper wordlen is ok and that using a wordlen greater than the
    input text fails.

--- test/poettest.c
+++ test/poettest.c
@@ -1,11 +1,26 @@
 #include <glib.h>
 
+#include "poet.h"
+
 void test_wordlen() {
-  g_assert(42 == 42);
+  Poet *p = NULL;
+  p = Poet_new("Frobisher", 3);
+  g_assert(p != NULL);
 }
 
-int main(int argc, char **argv){
+void test_wordlen_fail() {
+  if (g_test_subprocess()) {
+    Poet *p = NULL;
+    p = Poet_new("Frodo", 9);
+  }
+  g_test_trap_subprocess(NULL, 0, 0);
+  g_test_trap_assert_failed();
+  g_test_trap_assert_stderr("\nText to short for given word length\n");
+}
+
+int main(int argc, char **argv) {
     g_test_init(&argc, &argv, NULL);
     g_test_add_func("/set1/test wordlen", test_wordlen);
+    g_test_add_func("/set1/test wordlen fail", test_wordlen_fail);
     return g_test_run();
 }
